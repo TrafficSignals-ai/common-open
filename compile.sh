f48406c7ca4751f2d84fcaa8f5fb094b2cc49cab
Richard Thurbin <75902830+RichardThurbin@users.noreply.github.com>
2021-09-27 17:25:21 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,16 +5,16 @@ echo "-----------------------------------------------"
 phycores=$(echo $sudoPW|cat - /proc/cpuinfo|grep -m 1 "cpu cores"|awk '{print $ 4;}')
 echo "-- Cores: $phycores."
 
-rm -rf      /release
-mkdir -p    /release/include
-cp -r       /include/*                       /release/include
-mkdir -p    /release/log
+rm -rf      release
+mkdir -p    release/include
+cp -r       include/*                       release/include
+mkdir -p    release/log
 echo "-- Refreshed /release"
 
-rm -rf      /debug
-mkdir -p    /debug/include
-cp -r       /include/*                       /debug/include
-mkdir -p    /debug/log
+rm -rf      debug
+mkdir -p    debug/include
+cp -r       include/*                       debug/include
+mkdir -p    debug/log
 echo "-- Refreshed /release"
 
 
